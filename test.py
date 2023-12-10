cff1aad6759bd0a69fa898ba0e3ee8e41a0cf992
Alexander Li <li420087@gmail.com>
2023-12-11 10:02:06 +1100

    Fixed the training script for 1D world.

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-# created by Alexander Li
-# student ID: 30630711
-# date: 2023-12-6
+# Write a bubble sort function
+# that takes in a list and returns a sorted list.
+# Use the test code below to check your function.
+# print(bubble_sort([37, 42, 9, 19, 35, 4, 53, 22]))
+# print(bubble_sort([5, 4, 3, 2, 1]))
 
-import numpy as np
-import matplotlib.pyplot as plt
+def bubblesort(input_list):
+    for i in range(len(input_list)):
+        for j in range(len(input_list)-1):
+            if input_list[j] > input_list[j+1]:
+                input_list[j], input_list[j+1] = input_list[j+1], input_list[j]
+    return input_list
 
+print(bubblesort([37, 42, 9, 19, 35, 4, 53, 22]))
+print(bubblesort([5, 4, 3, 2, 1]))
